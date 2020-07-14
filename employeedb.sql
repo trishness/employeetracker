c9faefff23cc42d64756223a53bc6c54c6370f8f
Trish Ness <trishnss@gmail.com>
2020-07-13 18:30:46 -0700

    initial tables set up, initial prompts set up, npm installs done

---
+++ employeedb.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30), NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM role;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM employee;
---
+++ index.js
@@ -0,0 +1,151 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "Pagliachii3#",
+
+    database: "employee_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadID);
+    init();
+});
+
+function init() {
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add a department", "Add a role", "Add an employee", "Update employee roles", "Exit"],
+            name: "init"
+        }
+    )
+    .then(function(response){
+        console.log(response);
+
+        if(response.init === "Add a department") {
+            inquirer.prompt(
+                {
+                    type: "input",
+                    message: "What is the name of the department?",
+                    name: "departmentName"
+                }
+            )
+            .then(function(departmentAnswer){
+                console.log(departmentAnswer);
+                connection.query(
+                    "INSERT INTO department SET ?",
+                    {
+                        name: departmentAnswer.departmentName
+                    },
+                    function(err,res) {
+                        if (err) throw err;
+                        console.log(res.affectedRow + " department added!\n");
+                    }
+                ); init();
+            })
+        }
+        else if(response.init === "Add a role"){
+            inquirer.prompt(
+                [
+                    {
+                        type: "input",
+                        message: "What is the title of the role?",
+                        name: "roleTitle"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the salary of the role?",
+                        name: "roleSalary"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the department ID that the role is being added to?",
+                        name: "roleDepartment"
+                    }
+                ]
+            )
+            .then(function(roleAnswer){
+                console.log(roleAnswer);
+                connection.query(
+                    "INSERT INTO role SET ?",
+                    {
+                        title: roleAnswer.roleTitle,
+                        salary: roleAnswer.roleSalary,
+                        department_id: roleAnswer.roleDepartment
+                    },
+                    function(err,res){
+                        if (err) throw err;
+                        console.log(res.affectedRow + " role added!\n");
+                    }
+                ); init();
+            })
+        }
+        else if(response.init === "Add an employee") {
+            inquirer.prompt(
+                [
+                    {
+                        type: "input",
+                        message: "What is the employee's first name?",
+                        name: "empFirstName"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the employee's last name?",
+                        name: "empLastName"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the role ID of this employee?",
+                        name: "empRole"
+                    },
+                    {
+                        type: "input",
+                        message: "What is this employee's manager's ID? If no manager, please type '0'",
+                        name: "empManager"
+                    }
+                ]
+            )
+            .then(function(empAnswer){
+                console.log(empAnswer);
+                connection.query(
+                    "INSERT INTO employee SET ?",
+                    {
+                        first_name: empAnswer.empFirstName,
+                        last_name: empAnswer.empLastName,
+                        role_id: empAnswer.empRole,
+                        manager_id: empAnswer.empManager
+                    },
+                    function(err,res){
+                        if (err) throw err;
+                        console.log(res.affectedRow + " employee added!\n")
+                    }
+                ); init();
+            })
+        }
+        else {
+            console.log("Selecting all departments, roles and employees...\n");
+            connection.query("SELECT * FROM department", function(err, res){
+                if(err) throw err;
+                console.table(res);
+            });
+            connection.query("SELECT * FROM role", function(err,res){
+                if(err) throw err;
+                console.table(res);
+            });
+            connection.query("SELECT * FROM employee", function(err,res){
+                if(err) throw err;
+                console.table(res);
+            })
+            connection.end();
+        }
+    })
+}
